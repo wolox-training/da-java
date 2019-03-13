edcc5d817065767f85ce23f3bf18a7b4f802fafa
darwin araque <ingdarwinaraque@gmail.com>
2019-03-13 16:45:18 -0500

    Refactor to set change of a list

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -1,17 +1,16 @@
 package wolox.training.models;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import wolox.training.exceptions.BookAlreadyOwnedException;
@@ -37,12 +36,8 @@ public class User {
     @Column(nullable = false)
     private LocalDate birthDate;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "book_user",
-        joinColumns = @JoinColumn(name = "book_id", referencedColumnName = "id"),
-        inverseJoinColumns = @JoinColumn(name = "user_id",
-            referencedColumnName = "id"))
-    private List<Book> books = new ArrayList<>();
+    @ManyToMany(cascade = {CascadeType.REFRESH, CascadeType.MERGE})
+    private Set<Book> books = new HashSet();
 
     public long getId() {
         return id;
@@ -76,18 +71,16 @@ public class User {
         this.birthDate = birthDate;
     }
 
-    public List<Book> getBooks() {
-        return (List<Book>) Collections.unmodifiableCollection(books);
+    public Set<Book> getBooks() {
+        return (Set<Book>) Collections.unmodifiableCollection(books);
     }
 
-    public void setBooks(List<Book> books) {
+    public void setBooks(Set<Book> books) {
         this.books = books;
     }
 
     public void addBook(Book book) {
-       if (this.books.contains(book)){
-            this.books.add(book);
-        } else {
+       if (!this.books.add(book)){
            Exception core = new RuntimeException("Unique constrains violated for book_user");
            throw new BookAlreadyOwnedException("Book already relationated to the user", core);
        }
